1a804166cf8204b3994c1bc1f2edce2f550f0861
rohan <rohan@Rohans-MacBook-Air.local>
2018-12-13 14:18:36 -0800

    Added code for distance mesurement using 2 ultrasonic sensors

--- ultrasonic_sensor.c
+++ ultrasonic_sensor.c
@@ -1,83 +1,220 @@
-#include "ultrasonicSensor.h"
+/*****************************************************
+ * using this library
+ * From main call
+ * uss_setup_pins() finction
+ *
+ * then call uss_measure_distance() function
+ * the measure distance function returns a uint32_t
+ *
+ *****************************************************/
+
+#include "ultrasonic_sensor.h"
+
+
+
+
 
 //This is to avoid doing the math everytime you do a reading
 const double temp = 1.0/80.0;
 
 //Stores the pulse length
-volatile uint32_t pulse=0;
+volatile uint32_t pulse_1 = 0 ;
+
+//Stores the pulse length
+volatile uint32_t pulse_2 = 0 ;
 
 //Tells the main code if the a pulse is being read at the moment
-volatile uint8_t echowait=0;
+volatile uint8_t echowait_1 =0;
 
-void setupUltrasonic(){
-    ConfigureUART();
+volatile uint8_t echowait_2 =0;
+
+
+
+
+
+/* Add this to startup.c
+extern inputInt(void);
+input_int, for the GPIO handler
+
+*/
+
+
+void uss_setup_pins_1()
+{
+
     //Configures the timer
-    ConfigureTimer();
+     ConfigureTimer_1();
 
-    //setting up the Trigger pin
-    SysCtlPeripheralEnable(sys_trigger_port);
-    SysCtlDelay(3);
-    GPIOPinTypeGPIOOutput(trigger_port, trigger_pin);
+     //setting up the Trigger pin
+     SysCtlPeripheralEnable(sys_trigger_port_1);
+     SysCtlDelay(3);
+     GPIOPinTypeGPIOOutput(trigger_port_1, trigger_pin_1);
 
-    //setting up the Echo pin
-    SysCtlPeripheralEnable(sys_echo_port);
-    SysCtlDelay(3);
-    GPIOPinTypeGPIOInput(echo_port, echo_pin);
-    GPIOIntEnable(echo_port, echo_pin);
-    GPIOIntTypeSet(echo_port, echo_pin,GPIO_BOTH_EDGES);
-    GPIOIntRegister(echo_port,inputInt);
+     //setting up the Echo pin
+     SysCtlPeripheralEnable(sys_echo_port_1);
+     SysCtlDelay(3);
+     GPIOPinTypeGPIOInput(echo_port_1, echo_pin_1);
+     GPIOIntEnable(echo_port_1, echo_pin_1);
+     GPIOIntTypeSet(echo_port_1, echo_pin_1,GPIO_BOTH_EDGES);
+     GPIOIntRegister(echo_port_1,echo_int_1);
+
 }
 
-void checkBlindSpot(){
+
+uint32_t uss_measure_distance_1()
+{
 
     //Checks if a pulse read is in progress
-    if(echowait != 1){
-      //Does the required pulse of 10uS
-      GPIOPinWrite(trigger_port, trigger_pin, trigger_pin);
-      SysCtlDelay(266);
-      GPIOPinWrite(trigger_port, trigger_pin, ~trigger_pin);
+    if(echowait_1 != 1)
+    {
+    //Does the required pulse of 10uS
+    GPIOPinWrite(trigger_port_1, trigger_pin_1, trigger_pin_1);
+    SysCtlDelay(266);
+    GPIOPinWrite(trigger_port_1, trigger_pin_1, ~trigger_pin_1);
 
 
-      while(echowait != 0); // wait until the echo pin goes low
+    while(echowait_1 != 0); // wait until the echo pin goes low
 
-      //Converts the counter value to cm.
-      pulse =(uint32_t)(temp * pulse);
-      pulse = pulse / 58;
-
-      //Prints out the distance measured.
-      //UARTprintf("distance = %2dcm \n" , pulse);
+    //Converts the counter value to cm.
+    pulse_1 =(uint32_t)(temp * pulse_1);
+    pulse_1 = pulse_1 / 58;
+    return (pulse_1);
     }
-      //wait about 10ms until the next reading.
-      SysCtlDelay(400000);
-
 
 }
 
-void inputInt(){
-  //Clear interrupt flag. Since we only enabled on this is enough
-  GPIOIntClear(echo_port, echo_pin);
 
-  /*
-    If it's a rising edge then set he timer to 0
-    It's in periodic mode so it was in some random value
-  */
-  if ( GPIOPinRead(echo_port, echo_pin) == echo_pin){
-    HWREG(TIMER2_BASE + TIMER_O_TAV) = 0; //Loads value 0 into the timer.
-    TimerEnable(TIMER2_BASE,TIMER_A);
-    echowait=1;
-  }
-  /*
+void echo_int_1()
+{
+    //Clear interrupt flag. Since we only enabled on this is enough
+    GPIOIntClear(echo_port_1, echo_pin_1);
+
+    /*
+        If it's a rising edge then set he timer to 0
+        It's in periodic mode so it was in some random value
+     */
+    if ( GPIOPinRead(echo_port_1, echo_pin_1) == echo_pin_1)
+    {
+        HWREG(TIMER1_BASE + TIMER_O_TAV) = 0; //Loads value 0 into the timer.
+        TimerEnable(TIMER1_BASE,TIMER_A);
+        echowait_1=1;
+    }
+    /*
     If it's a falling edge that was detected, then get the value of the counter
-  */
-  else{
-    pulse = TimerValueGet(TIMER2_BASE,TIMER_A); //record value
-    TimerDisable(TIMER2_BASE,TIMER_A);
-    echowait=0;
-  }
-
+     */
+    else
+    {
+        pulse_1 = TimerValueGet(TIMER1_BASE,TIMER_A); //record value
+        TimerDisable(TIMER1_BASE,TIMER_A);
+        echowait_1=0;
+    }
 
 }
-void ConfigureTimer(){
+
+
+void ConfigureTimer_1()
+{
+  /*
+    Set the timer to be periodic.
+  */
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER1);
+  SysCtlDelay(3);
+  TimerConfigure(TIMER1_BASE, TIMER_CFG_PERIODIC_UP);
+  TimerEnable(TIMER1_BASE,TIMER_A);
+}
+
+
+
+
+//
+//
+//int main(){
+//
+//    uss_setup_pins();
+//    uint32_t dist = measure_distance();
+//}
+
+
+
+
+
+
+
+void uss_setup_pins_2()
+{
+
+    //Configures the timer
+     ConfigureTimer_2();
+
+     //setting up the Trigger pin
+     SysCtlPeripheralEnable(sys_trigger_port_2);
+     SysCtlDelay(3);
+     GPIOPinTypeGPIOOutput(trigger_port_2, trigger_pin_2);
+
+     //setting up the Echo pin
+     SysCtlPeripheralEnable(sys_echo_port_2);
+     SysCtlDelay(3);
+     GPIOPinTypeGPIOInput(echo_port_2, echo_pin_2);
+     GPIOIntEnable(echo_port_2, echo_pin_2);
+     GPIOIntTypeSet(echo_port_2, echo_pin_2,GPIO_BOTH_EDGES);
+     GPIOIntRegister(echo_port_2,echo_int_2);
+
+}
+
+
+uint32_t uss_measure_distance_2()
+{
+
+    //Checks if a pulse read is in progress
+    if(echowait_2 != 1)
+    {
+    //Does the required pulse of 10uS
+    GPIOPinWrite(trigger_port_2, trigger_pin_2, trigger_pin_2);
+    SysCtlDelay(266);
+    GPIOPinWrite(trigger_port_2, trigger_pin_2, ~trigger_pin_2);
+
+
+    while(echowait_2 != 0); // wait until the echo pin goes low
+
+    //Converts the counter value to cm.
+    pulse_2 =(uint32_t)(temp * pulse_2);
+    pulse_2 = pulse_2 / 58;
+    return (pulse_2);
+    }
+
+}
+
+
+void echo_int_2()
+{
+    //Clear interrupt flag. Since we only enabled on this is enough
+    GPIOIntClear(echo_port_2, echo_pin_2);
+
+    /*
+        If it's a rising edge then set he timer to 0
+        It's in periodic mode so it was in some random value
+     */
+    if ( GPIOPinRead(echo_port_2, echo_pin_2) == echo_pin_2)
+    {
+        HWREG(TIMER2_BASE + TIMER_O_TAV) = 0; //Loads value 0 into the timer.
+        TimerEnable(TIMER2_BASE,TIMER_A);
+        echowait_1=1;
+    }
+    /*
+    If it's a falling edge that was detected, then get the value of the counter
+     */
+    else
+    {
+        pulse_2 = TimerValueGet(TIMER2_BASE,TIMER_A); //record value
+        TimerDisable(TIMER2_BASE,TIMER_A);
+        echowait_2=0;
+    }
+
+}
+
+
+void ConfigureTimer_2()
+{
   /*
     Set the timer to be periodic.
   */
@@ -88,18 +225,5 @@ void ConfigureTimer(){
 }
 
 
-void ConfigureUART(void) {
-    // Enable the GPIO Peripheral used by the UART.
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-    // Enable UART0
-    ROM_SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
-    // Configure GPIO Pins for UART mode.
-    ROM_GPIOPinConfigure(GPIO_PA0_U0RX);
-    ROM_GPIOPinConfigure(GPIO_PA1_U0TX);
-    ROM_GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
-    // Use the internal 16MHz oscillator as the UART clock source.
-    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
-    // Initialize the UART for console I/O.
-    UARTStdioConfig(0, 115200, 16000000);
-}
+
 
