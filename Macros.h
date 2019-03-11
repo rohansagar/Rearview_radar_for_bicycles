cb8d04a4071354079bcb010be29f813279c826b2
rohan <rohan@sfs-wifi-dhcp-10-143-229-144.wifi.sfsu.edu>
2019-03-11 00:12:36 -0700

    removed old files

--- Macros.h
+++
@@ -1,6 +0,0 @@
-#define ONE_US  (uint32_t)(SysCtlClockGet() / (1000*1000)) // One microsecond
-#define ONE_MS  (uint32_t)(SysCtlClockGet() / 1000) // One millisecond
-#define ONE_SEC (uint32_t)(SysCtlClockGet()) // One second
-
-#define ONE_MS_PIOSC    16000
-#define ONE_SEC_PIOSC   16000000
